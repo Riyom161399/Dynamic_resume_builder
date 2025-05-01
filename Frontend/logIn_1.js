738f868da4536b5dc338ebd19dda57f67b2ec3f3
Joy chowdhury <cjoy882@gmail.com>
2025-05-01 15:50:17 +0600

    Create logIn_1.js

---
+++ Frontend/logIn_1.js
@@ -0,0 +1,7 @@
+document.getElementById("login").addEventListener("click", function () {
+  window.location.href = "signIn.html";
+});
+
+document.getElementById("signup").addEventListener("click", function () {
+  window.location.href = "signUp.html";
+});
