8ef3ea49377f94550c51781aabd92e59ebfb080d
Isnat Sami <isnatsamiims7@gmail.com>
2025-07-09 00:40:13 +0600

    Deleted Backend

--- Backend/routes/jobRoutes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const { getJobs, postJob, applyToJob } = require('../controllers/jobController');
-
-router.get('/', getJobs);
-router.post('/', postJob);
-router.post('/:id/apply', applyToJob);
-
-module.exports = router;
-
--- Backend/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const { getUser, updateUser } = require('../controllers/userController');
-
-router.get('/:id', getUser);
-router.put('/:id', updateUser);
-
-module.exports = router;
-// This code defines the user routes for getting and updating user information.
